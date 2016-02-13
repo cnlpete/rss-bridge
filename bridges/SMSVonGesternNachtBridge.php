aad2b363843fba0597433ae9e2b0fdbb438943b4
Hauke Schade <cnlpete@cnlpete.de>
2016-02-14 00:09:58 +0100

    upgraded for new AbstractBridge

--- bridges/SMSVonGesternNachtBridge.php
+++ bridges/SMSVonGesternNachtBridge.php
@@ -1,44 +1,45 @@
 <?php
-/**
-*
-* @name SMSVonGesternNachtBridge
-* @homepage http://www.smsvongesternnacht.de/
-* @description The Unofficial SMSVonGesternNacht Bridge
-* @update 04/11/2015
-* @maintainer cnlpete
-*/
 class SMSVonGesternNachtBridge extends BridgeAbstract {
 
-    public function collectData(array $param) {
-        $html = file_get_html('http://www.smsvongesternnacht.de/') or $this->returnError('Could not request SMSVonGesternNacht.', 404);
+  public function loadMetadatas() {
+		$this->maintainer = "cnlpete";
+		$this->name = "SMSVonGesternNachtBridge";
+		$this->uri = "http://www.smsvongesternnacht.de/";
+		$this->description = "The Unofficial SMSVonGesternNacht Bridge";
+		$this->update = "2016-02-13";
+	}
 
-        foreach($html->find('article.sms-thread') as $element) {
-            $contents = [];
-            foreach($element->find('div.field-items', 0)->find('.sms-participant') as $entry) {
-                $contents[] = $entry;
-            }
+  public function collectData(array $param) {
+    $html = file_get_html('http://www.smsvongesternnacht.de/') or $this->returnError('Could not request SMSVonGesternNacht.', 404);
 
-            $item = new Item();
-            $item->uri = $element->find('a.sms-fb-url', 0)->href;
-            $item->content = join(' ', $contents);
-            $item->title = $element->find('.node-title', 0)->plaintext;
-            $this->items[] = $item;
-        }
+    foreach($html->find('article.sms-thread') as $element) {
+      $contents = [];
+
+      foreach($element->find('div.field', 0)->find('.sms-participant') as $entry) {
+        $contents[] = $entry;
+      }
+
+      $item = new Item();
+      $item->uri = $element->find('a.sms-fb-url', 0)->href;
+      $item->content = join(' ', $contents);
+      $item->title = $element->find('.node-title', 0)->plaintext;
+      $this->items[] = $item;
     }
+  }
 
-    public function getName() {
-        return 'SMSVonGesternNacht';
-    }
+  public function getName() {
+    return 'SMSVonGesternNacht';
+  }
 
-    public function getURI() {
-        return 'http://www.smsvongesternnacht.de/';
-    }
+  public function getURI() {
+    return 'http://www.smsvongesternnacht.de/';
+  }
 
-    public function getDescription() {
-        return 'SMSVonGesternNacht via rss-bridge';
-    }
+  public function getDescription() {
+    return 'SMSVonGesternNacht via rss-bridge';
+  }
 
-    public function getCacheDuration() {
-        return 1 * 3600; // 1 hour
-    }
+  public function getCacheDuration() {
+    return 1 * 3600; // 1 hour
+  }
 }
