4be62ec30acb6064d6a6b3ba639637819bd01953
Hauke Schade <cnlpete@cnlpete.de>
2016-02-14 00:10:24 +0100

    smsvongesternnacht ist now chatvongesternnacht

--- bridges/SMSVonGesternNachtBridge.php
+++ bridges/SMSVonGesternNachtBridge.php
@@ -4,13 +4,13 @@ class SMSVonGesternNachtBridge extends BridgeAbstract {
   public function loadMetadatas() {
 		$this->maintainer = "cnlpete";
 		$this->name = "SMSVonGesternNachtBridge";
-		$this->uri = "http://www.smsvongesternnacht.de/";
+		$this->uri = "http://www.chatvongesternnacht.de/";
 		$this->description = "The Unofficial SMSVonGesternNacht Bridge";
 		$this->update = "2016-02-13";
 	}
 
   public function collectData(array $param) {
-    $html = file_get_html('http://www.smsvongesternnacht.de/') or $this->returnError('Could not request SMSVonGesternNacht.', 404);
+    $html = file_get_html('http://www.chatvongesternnacht.de/') or $this->returnError('Could not request SMSVonGesternNacht.', 404);
 
     foreach($html->find('article.sms-thread') as $element) {
       $contents = [];
